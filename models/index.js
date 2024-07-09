b9dcaa8919793960825016160f0568b2d3f60e3b
Eric <ekellogg90@gmail.com>
2024-07-09 18:34:25 -0500

    comment seeds

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const BlogPost = require('./blogPost');
-//const Comment = require('./comment');
+const Comment = require('./comment');
 
 User.hasMany(BlogPost, {
   foreignKey: 'user_id',
@@ -11,23 +11,23 @@ BlogPost.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE',
-// });
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
-// Comment.belongsTo(User, {
-//   foreignKey: 'user_id',
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
-// BlogPost.hasMany(Comment, {
-//   foreignKey: 'blogPost_id',
-//   onDelete: 'CASCADE',
-// });
+BlogPost.hasMany(Comment, {
+  foreignKey: 'blogPost_id',
+  onDelete: 'CASCADE',
+});
 
-// Comment.belongsTo(BlogPost, {
-//   foreignKey: 'blogPost_id',
-//   onDelete: 'CASCADE',
-// });
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'blogPost_id',
+  onDelete: 'CASCADE',
+});
 
-module.exports = { User, BlogPost };
+module.exports = { User, BlogPost, Comment };
